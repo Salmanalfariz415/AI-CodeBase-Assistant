3a8550e53ab521e3593585f1fe8ec842902d459d
Salmanalfariz-techz <Salmanalfariz332@gmail.com>
2026-07-14 00:01:51 +0530

    Intead of using axios and cheerio shift to using git clone to clone into a new folder created using the current timestamp

--- backend/controllers/gitcontrol.js
+++ backend/controllers/gitcontrol.js
@@ -1,32 +1,26 @@
-const axios = require('axios');//for reaching github
-const cheerio = require('cheerio');//text parsing
+const {exec}=require('child_process');
+const path = require('path');
 
 const uploader = async (req, res) => {
     const { url } = req.body;
-    try {
-        const response = await axios.get(url, {
-            headers: { 'User-Agent': 'Mozilla/5.0' } 
+    const folderName = `repo-${Date.now()}`;
+    const targetPath = path.join(__dirname, '../cloned_repos', folderName);
+    const command = `git clone ${url} ${targetPath}`;
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Exec Error: ${error.message}`);
+            return res.status(500).json({ 
+                error: "Failed to clone repository.",
+                details: error.message 
+            });
+        }
+        res.status(200).json({
+            message: "Repository successfully cloned",
+            folderName: folderName,
+            localPath: targetPath
         });
-        const html = response.data;
-
-        const $ = cheerio.load(html);
-
-        const pageTitle = $('title').text();
-        const mainHeading = $('h1').text().trim();
-
-        res.status(200).json({ 
-            message: "Link read successfully!", 
-            url: url,
-            scrapedData: {
-                title: pageTitle,
-                heading: mainHeading
-            }
-        });
-
-    } catch (error) {
-        console.error("Error reading the link:", error.message);
-        res.status(500).json({error: "Failed to read the link."});
-    }
+    });
+    
 };
 
 module.exports = { uploader };
